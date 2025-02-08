1f1484130c94360f223f6d61b86014c911c42e19
Animeshnik94 <almondmilk9119@gmail.com>
2025-02-08 03:04:16 +0300

    Admin panel setup

--- goods/admin.py
+++ goods/admin.py
@@ -5,12 +5,31 @@ from goods.models import Category, Product, Grower
 @admin.register(Category)
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('name',)}
+    list_display = ["name",]
 
 @admin.register(Product)
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('name',)}
+    list_display = ["name", "quantity", "price", "discount"]
+    list_editable = ["discount", ]
+    search_fields = ["name", "description", "grower__name"]
+    list_filter = ["category", "grower__name"]
+    readonly_fields = ["indica_share"]
+    fields = [
+        "name",
+        "category",
+        "grower",
+        "slug",
+        "description",
+        "image",
+        ("price", "discount"),
+        "quantity",
+        "flowering_time",
+        ("sativa_share", "indica_share"),
+        "thc"
+    ]
 
 @admin.register(Grower)
 class GrowerAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('name',)}
-    
+    list_display = ["name",]
