1595297939ebfd707c1f4714879b1ee13256873a
unknown <animeshnik94>
2025-02-16 18:07:44 +0300

    Commit before major changes

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.core.cache import cache
 from django.db.models import Prefetch
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
@@ -10,6 +11,7 @@ from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.generic import CreateView, DetailView, UpdateView, TemplateView
 
+from common.mixins import CacheMixin
 from orders.models import Order, OrderItem
 from carts.models import Cart
 from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
@@ -23,7 +25,7 @@ class UserLoginView(LoginView):
     def get_success_url(self):
         redirect_page = self.request.POST.get('next', None)
         if redirect_page and redirect_page != reverse('user:logout'):
-            return redirect_page #Если неавторизованый пользователь решит перейти сразу в профиль, его перекинет на
+            return redirect_page          #Если неавторизованый пользователь решит перейти сразу в профиль, его перекинет на
         return reverse_lazy('main:index') # страницу авторизации, после авторизации на его страницу
 
     def form_valid(self, form):
@@ -69,8 +71,8 @@ class UserRegistrationView(CreateView):
         context['title'] = 'Seed Shop - Регистрация'
         return context
 
-class UserProfileView(LoginRequiredMixin, UpdateView): # Login Required нужен для того
-                                                       # чтобы мог зайти только авторизованый юзер
+class UserProfileView(LoginRequiredMixin, CacheMixin,UpdateView): # Login Required Mixin нужен для того
+                                                                  # чтобы мог зайти только авторизованый юзер
     template_name = 'users/profile.html'
     form_class = ProfileForm
     success_url = reverse_lazy('users:profile')
@@ -89,12 +91,15 @@ class UserProfileView(LoginRequiredMixin, UpdateView): # Login Required нуже
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Seed Shop - Кабинет'
-        context['orders'] = Order.objects.filter(user=self.request.user).prefetch_related(
+
+        orders = Order.objects.filter(user=self.request.user).prefetch_related(
             Prefetch(
                 "orderitem_set",
                 queryset=OrderItem.objects.select_related("product")
             )
         ).order_by("-id")
+
+        context['orders'] = self.set_get_cache(orders, f"user_{self.request.user.id}_orders", 60 * 2)
         return context
 
 class UserCartView(TemplateView):
@@ -111,11 +116,12 @@ class LogoutView(View):
         messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
         auth.logout(request)
         return redirect(reverse('main:index'))
-@login_required
-def logout(request):
-    messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
-    auth.logout(request)
-    return redirect(reverse('main:index'))
+
+# @login_required
+# def logout(request):
+#     messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
+#     auth.logout(request)
+#     return redirect(reverse('main:index'))
 
 # def users_cart(request):
 #     context = {
